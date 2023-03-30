f200625d7b647921c07c2342c8fc10bd60b0655b
Denis Voroniuc <122531721+Hajima-VD@users.noreply.github.com>
2023-03-31 00:40:51 +0300

    codewars 31_04_23

--- draft.js
+++ draft.js
@@ -1,2 +1,15 @@
 // напиши решение на js  и подробно поясни на русском языке
+const myNumber = 5.3;
+if(Number.isInteger(myNumber)) {
+  console.log(`${myNumber} is an integer`);
+} else {
+  console.log(`${myNumber} is not an integer`);
+}
 
+
+function findNextSquare(sq) {
+  const sqrt = sq ** 0.5;
+  return Number.isInteger(sqrt) ? (sqrt+1)**2 : -1 ;
+}
+
+console.log(findNextSquare(122))
