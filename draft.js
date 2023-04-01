93c1e0ed12860dff8d042099000419bd286e5b98
Denis Voroniuc <122531721+Hajima-VD@users.noreply.github.com>
2023-04-01 23:02:29 +0300

    codewars 01_04_23

--- draft.js
+++ draft.js
@@ -1,15 +1,16 @@
 // напиши решение на js  и подробно поясни на русском языке
-const myNumber = 5.3;
-if(Number.isInteger(myNumber)) {
-  console.log(`${myNumber} is an integer`);
-} else {
-  console.log(`${myNumber} is not an integer`);
-}
+function minMax(arr){
+  const rezult = arr.sort(function(a, b) {
+         return a - b;});
+   return newArray = [rezult[0],rezult[rezult.length-1]];
+ }
+console.log(minMax([23 , 11 ,-11,-3 , 3  ]));
 
 
-function findNextSquare(sq) {
-  const sqrt = sq ** 0.5;
-  return Number.isInteger(sqrt) ? (sqrt+1)**2 : -1 ;
-}
+To create a new array in JavaScript containing the first and last elements of an existing array, you can use the slice() method. First, you can create a variable to store the original array, then use the slice() method to return a new array containing the first and last elements. Here's an example of how to do this:
+let originalArray = [1, 2, 3, 4, 5];
+let newArray = [originalArray[0], originalArray.slice(-1)[0]];
+console.log(newArray); // [1, 5]
 
-console.log(findNextSquare(122))
+In this example, originalArray[0] retrieves the first element of originalArray, while originalArray.slice(-1)[0] retrieves the last element. The slice() method with a negative index argument starts slicing from the end of the array, so slice(-1) returns an array with only the last element, and [0] retrieves that element from
+length
