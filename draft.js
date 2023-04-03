38f06fe4ff8e3826153f9ab6ac3ca9456c32bc0c
Denis Voroniuc <122531721+Hajima-VD@users.noreply.github.com>
2023-04-03 23:59:24 +0300

    codewars 03+04_23

--- draft.js
+++ draft.js
@@ -1,7 +1,39 @@
 // напиши решение на js  и подробно поясни на русском языке
 
-  function switchItUp(number){
-    return number==0 ? 'Zero' : number== 1 ? 'One' : number== 2 ? 'Two' : number==3 ? 'Three' : number==4 ? 'Four' : number==5 ? 'Five' : number==6? 'Six' : number==7 ? 'Seven' : number==8 ?  'Eight' : 'Nine' ;
-    }
+function persistence(num) {
+  while (num.length <1){
+    num.split('').reduce((acc, cur) => acc * cur);
+    console.log(num);
+  console.log(num.length);
+  if (num.length <=1 ) {
+    return 0 ;
+  } else {
+  }  
+}
+}
+console.log(persistence('34343')); 
 
-console.log(switchItUp(0)); 
+function persistence(num) {
+  let arr = num;
+  while (arr.length >1){
+  arr=num.split('').reduce((acc, cur) => acc*cur);
+  console.log(arr);}
+}
+console.log(persistence('443434')); 
+
+
+function persistence(num) {
+  let arr = num;
+  let count = 0
+ if (arr.length<=1){
+  return 0
+ } else {
+  while (arr.length > 1) {
+    arr = arr.split('').reduce((acc, cur) => acc * cur).toString();
+    count++;
+  }
+  
+  return count;
+ }
+}
+console.log(persistence('234234234')); // 2
