523c32b1b243cc1bba4dbc280a911dd9c4391f38
Denis Voroniuc <122531721+Hajima-VD@users.noreply.github.com>
2023-04-07 00:16:14 +0300

    codewars 07_04_23

--- draft.js
+++ draft.js
@@ -1,39 +1,8 @@
 // напиши решение на js  и подробно поясни на русском языке
 
-function persistence(num) {
-  while (num.length <1){
-    num.split('').reduce((acc, cur) => acc * cur);
-    console.log(num);
-  console.log(num.length);
-  if (num.length <=1 ) {
-    return 0 ;
-  } else {
-  }  
-}
-}
-console.log(persistence('34343')); 
-
-function persistence(num) {
-  let arr = num;
-  while (arr.length >1){
-  arr=num.split('').reduce((acc, cur) => acc*cur);
-  console.log(arr);}
-}
-console.log(persistence('443434')); 
-
-
-function persistence(num) {
-  let arr = num;
-  let count = 0
- if (arr.length<=1){
-  return 0
- } else {
-  while (arr.length > 1) {
-    arr = arr.split('').reduce((acc, cur) => acc * cur).toString();
-    count++;
+function filter_list(l) {
+   
+  const arr = l.filter(a => a == a.isNaN);
+  return arr;
   }
-  
-  return count;
- }
-}
-console.log(persistence('234234234')); // 2
+console.log(filter_list([1,2,'aasf','1','123',123])); // 2
