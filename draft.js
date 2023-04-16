a216dec7bb20e6eed3cf941c952c49ee5029e6d6
Denis Voroniuc <122531721+Hajima-VD@users.noreply.github.com>
2023-04-16 22:33:37 +0300

    codewars 16_04_23

--- draft.js
+++ draft.js
@@ -1,8 +1,23 @@
 // напиши решение на js  и подробно поясни на русском языке
 
-function filter_list(l) {
-   
-  const arr = l.filter(a => a == a.isNaN);
-  return arr;
+function powersOfTwo(n){
+  let arr = [];
+  for (let i=0 ; i<=n ; i++){
+    arr=arr+i
   }
-console.log(filter_list([1,2,'aasf','1','123',123])); // 2
+ return arr.split('').map(item =>{return Math.pow(2,item)});
+}
+
+
+Вот один из возможных вариантов решения этой задачи:
+
+function powersOfTwo(n) {
+  let arr = [];
+  for (let i = 0; i <= n; i++) {
+    arr.push(Math.pow(2, i)); // добавляем в массив степень двойки с текущим показателем
+  }
+  return arr; // возвращаем массив
+}
+
+console.log(powersOfTwo(4));
+
